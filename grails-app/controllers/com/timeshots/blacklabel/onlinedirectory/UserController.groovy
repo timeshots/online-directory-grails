f58ed6774b3ab88050d7422f92db9c58ecf929d9
redwolfgang24 <redwolfgang20@gmail.com>
2014-07-13 01:30:00 +0800

    UserController first Commit

---
+++ grails-app/controllers/com/timeshots/blacklabel/onlinedirectory/UserController.groovy
@@ -0,0 +1,11 @@
+package com.timeshots.blacklabel.onlinedirectory
+
+import grails.transaction.Transactional
+
+@Transactional(readOnly = true)
+class UserController {
+
+    def index(Integer max) {
+
+	}
+}
