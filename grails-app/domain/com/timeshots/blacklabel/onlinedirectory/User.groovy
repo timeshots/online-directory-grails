9f0d9766b166ee9307e27ca05e49dbc762cde912
redwolfgang24 <redwolfgang20@gmail.com>
2014-07-13 21:30:58 +0800

    online-directory-grails Fourth commit for updated changes

--- grails-app/domain/com/timeshots/blacklabel/onlinedirectory/User.groovy
+++ grails-app/domain/com/timeshots/blacklabel/onlinedirectory/User.groovy
@@ -2,53 +2,22 @@ package com.timeshots.blacklabel.onlinedirectory
 
 class User {
 
-	transient springSecurityService
-	String id
 	String firstName
 	String lastName
 	String emailAddress
 	String username
 	String password
 
-	static transients = ['springSecurityService']
-
     static constraints = {
-		firstName(nullable: false, blank: false)
-		lastName(nullable: false, blank: false)
-		emailAddress(nullable: false, blank: false, email: true)
+		firstName(nullable: false)
+		lastName(nullable: false)
+		emailAddress(nullable: false, email: true)
 		username(nullable: true, matches:"[a-zA-Z0-9]+")
 		password(nullable: false, password: true)
     }
 
-	static mapping = {
-		table 'users'
-		password column: '`password`'
-		id generator:"uuid"
-	}
-
-	def beforeInsert(){
-		if(password != null){
-			encodePassword()
-		}
-	}
-
-	def beforeUpdate(){
-		if(isDirty('password')){
-			encodePassword()
-		}
-	}
-
-	protected void ecodePassword(){
-		password = springSecurityService.encodePassword(password)
-	}
-
-	def boolean isCompletelyRegistered(){
-		boolean isCompletelyRegistered = false
-
-		if(username && password){
-			isCompletelyRegistered = true
-		}
-
-		return isCompletelyRegistered
+	@Override
+	String toString(){
+		return lastName
 	}
 }
